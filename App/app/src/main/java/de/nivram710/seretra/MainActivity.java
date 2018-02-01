6ee70182a9114d8a1e172a9c1ff1bd6bdb544d36
nivram710 <juettemarvin@gmail.com>
2018-02-01 17:34:58 +0100

    First Commit

---
+++ App/app/src/main/java/de/nivram710/seretra/MainActivity.java
@@ -0,0 +1,13 @@
+package de.nivram710.seretra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
