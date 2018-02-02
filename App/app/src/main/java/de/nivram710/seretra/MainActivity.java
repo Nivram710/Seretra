8590c14d76a95fe96f92d67ef8b71234b8d30d23
Nivram710 <juettemarvin@gmail.com>
2018-02-02 19:13:22 +0100

    Remove everythig, except permissions

--- App/app/src/main/java/de/nivram710/seretra/MainActivity.java
+++ App/app/src/main/java/de/nivram710/seretra/MainActivity.java
@@ -1,94 +1,50 @@
 package de.nivram710.seretra;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
-import android.os.Debug;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    LocationManager locationManager;
-    LocationListener locationListener;
+    private static final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1;
     TextView koordinatenText;
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        koordinatenText = findViewById(R.id.textField);
+        koordinatenText = findViewById(R.id.koordinaten);
         koordinatenText.setText(R.string.app_name);
 
-        getLocation();
-    }
-
-    // todo: Standort findung in den Griff bekommen
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Log.d("MainActivity", "Nutzer nach Rechten fragen");
-            requestPermissions(new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.INTERNET
-            }, 10);
-            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-            locationListener = new LocationListener() {
-                @SuppressLint("SetTextI18n")
-                @Override
-                public void onLocationChanged(Location location) {
-                    koordinatenText.setText("\n" + location.getLatitude() + " " + location.getLongitude());
-                }
-
-                @Override
-                public void onStatusChanged(String s, int i, Bundle bundle) {
-
-                }
-
-                @Override
-                public void onProviderEnabled(String s) {
-
-                }
-
-                @Override
-                public void onProviderDisabled(String s) {
-                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    startActivity(intent);
-                }
-            };
-        } else {
-            koordinatenText.setText(R.string.gps_permission_denied);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_COARSE_LOCATION },
+                    PERMISSIONS_ACCESS_COARSE_LOCATION);
         }
+
     }
 
-    // todo: Permissions regeln!
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
-            case 10:
-                Log.d("MainActivity", "Case 10");
+            case PERMISSIONS_ACCESS_COARSE_LOCATION:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getLocation();
+                    Toast.makeText(this, getString(R.string.gps_permission_user_accept), Toast.LENGTH_LONG).show();
                 } else {
-                    koordinatenText.setText(R.string.gps_permission_denied);
+                    Toast.makeText(this, getString(R.string.gps_permission_user_denied), Toast.LENGTH_LONG).show();
                 }
+                break;
         }
     }
+
+    // todo: GPS aktivierung herausfinden
+    // todo: Standort findung in den Griff bekommen
+
 }
