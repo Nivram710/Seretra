354f8b1f61a62f6770d98d2aeffbe1f3acbe0896
Nivram710 <juettemarvin@gmail.com>
2018-02-03 15:38:07 +0100

    Debug-Ausgaben wurden entfernt - Permission-Abfrage funktioniert - Ortung funktioniert - Hintergrundprozess muss noch erstellt werden - Methode sendLocationToServer muss noch erstellt werden

--- App/app/src/main/java/de/nivram710/seretra/MainActivity.java
+++ App/app/src/main/java/de/nivram710/seretra/MainActivity.java
@@ -14,7 +14,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
