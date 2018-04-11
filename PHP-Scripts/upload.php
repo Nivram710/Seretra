347a6ffd03009d19c479d4e6506d4bb59fbb2b48
Nivram710 <contact@nivram710.de>
2018-04-11 22:26:24 +0200

    Neues Verfahren Daten auszuwerten

--- PHP-Scripts/upload.php
+++ PHP-Scripts/upload.php
@@ -1,16 +1,28 @@
 <?php
 
-error_reporting(0);
-require "conn.php";
-
+$debug = true;
 $imei = $_GET["imei"];
 $longtitude = $_GET["longtitude"];
 $latitude = $_GET["latitude"];
+$time = round(microtime(true) * 1000);
 
-if ($inert = $db->query("INSERT INTO locations (imei, longtitude, latitude, time) VALUES ('$imei', '$longtitude', '$latitude', CURRENT_TIMESTAMP)")) {
-	echo "Success";
-} else {
-	die("Error: " . $db->error);
+if($debug) {
+	echo "IMEI: " . $imei;
+	echo "<br>----------------------<br>";
+	echo "Longtitude: " . $longtitude;
+	echo "<br>----------------------<br>";
+	echo "Latitude: " . $latitude;
+	echo "<br>----------------------<br>";
+	echo "Time: " . $time;
+	echo "<br>----------------------<br>";
+
+	// TODO: check if values are not empty
+	echo "python /home/pi/development/mint/seretra/Prediction/append.py" . " " . $imei . " " . $time . " " . $longtitude . " " . $latitude . " 2>&1 &";
+	exec("python /home/pi/development/mint/seretra/Prediction/append.py" . " " . $imei . " " . $time . " " . $longtitude . " " . $latitude . " 2>&1 &", $out);
+	echo "<br>Outputs:<br>";
+	foreach($out as $line) {
+		echo $line . "<br>";
+	}
 }
 
 ?>
