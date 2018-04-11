cff8c5affa8c8439bd68bda57da4bb967feeeaf2
timokoesters <timokoesters@protonmail.com>
2018-04-11 20:59:00 +0200

    Cleaned up append.py
    Forced attributes to be convertable to certain types:
    time: int
    x: float
    y: float

--- Prediction/append.py
+++ Prediction/append.py
@@ -6,5 +6,11 @@ time = sys.argv[2]
 x = sys.argv[3]
 y = sys.argv[4]
 
-with open(".data/" + sys.argv[1], "a") as f:
-    f.write(int(time) + " " + float(x) + " " + float(y) + "\n")
+int(time)
+float(x)
+float(y)
+
+path = ".data/" + sys.argv[1]
+
+with open(path, "a") as f:
+    f.write(time + " " + x + " " + y + "\n")
