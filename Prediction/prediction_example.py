4b2a1573a373eed32039ba7a444c84255a10871b
PlayLucky <timo.koesters@gmail.com>
2018-04-11 16:35:40 +0200

    Updated example to test collisions

--- Prediction/prediction_example.py
+++ Prediction/prediction_example.py
@@ -2,28 +2,50 @@ from vec2 import Vec2
 import random
 import matplotlib.pyplot as plt
 from predict import predict
+from interpolate import interpolate
 
 # Input Data; last is newest, second element is difference in time from start
-data = [(Vec2(0, 0), 0)]
 
-random.seed(28)
 
-for i in range(1, 10):
-    angle = random.randint(-50, 50)
-    length = random.random()+0.2
-    data.append((data[-1][0]+Vec2(data[-1][0].getAngle()+angle, length, True), i))
+def generate_random_data(start, number):
+    data = [(start, 0)]
+    for i in range(1, number):
+        angle = random.randint(-50, 50)
+        length = random.random()+0.2
+        data.append((data[-1][0]+Vec2(data[-1][0].getAngle()+angle, length, True), i))
+    return data
 
-for i in range(50):
-    data.append(predict(data, 0.5))
 
-fig, ax = plt.subplots()
+# Predict
+#for i in range(50):
+#    data.append(predict(data, 0.5))
 
-plt.axis("equal")
+# Needs constant time step
+def find_collision(o1, o2, threshold):
+    for pos1, pos2 in zip(o1, o2):
+        if(Vec2.distance(pos1[0], pos2[0]) < threshold):
+            return pos1
 
-xs = list(map(lambda d: d[0].x, data))
-ys = list(map(lambda d: d[0].y, data))
 
-plt.plot(xs, ys)
-for i, p in enumerate(xs):
-    plt.scatter(p, ys[i])
-plt.show()
+random.seed(0.925)
+
+obj1_data = interpolate(generate_random_data(Vec2(0, 0), 10), 0.1)
+obj2_data = interpolate(generate_random_data(Vec2(0, 1), 10), 0.1)
+
+for i in range(0, 100):
+
+    collision = find_collision(obj1_data, obj2_data, 0.1)
+    """if collision:
+
+        xs = list(map(lambda d: d[0].x, obj1_data))
+        ys = list(map(lambda d: d[0].y, obj1_data))
+
+        plt.plot(xs, ys)
+
+        xs = list(map(lambda d: d[0].x, obj2_data))
+        ys = list(map(lambda d: d[0].y, obj2_data))
+        plt.plot(xs, ys)
+
+        plt.scatter(collision[0].x, collision[0].y)
+
+        plt.show()"""
