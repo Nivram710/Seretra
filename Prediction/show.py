b2aab6491fc6c06458dc11e4996f43924e8903e1
timokoesters <timokoesters@protonmail.com>
2018-04-13 17:03:24 +0200

    Added program to show a plot with all points in .data

---
+++ Prediction/show.py
@@ -0,0 +1,32 @@
+from vec2 import Vec2
+import random
+import matplotlib.pyplot as plt
+import glob
+
+from predict import predict
+from interpolate import interpolate
+from collision import find_collision
+
+
+data_path = ".data/"
+
+objects = {}
+for filename in glob.glob(data_path + "*"):
+    name = filename.lstrip(data_path)
+    with open(filename, "r") as f:
+        for position in f:
+            parts = position.split()
+            time = int(parts[0])
+            x = float(parts[1])
+            y = float(parts[2])
+            objects.setdefault(name, [])
+            objects[name].append((Vec2(x, y), time))
+
+for obj in objects.values():
+    obj_interpolated = obj  #interpolate(obj, 100)
+    xs = list(map(lambda d: d[0].x, obj_interpolated))
+    ys = list(map(lambda d: d[0].y, obj_interpolated))
+
+    plt.scatter(xs, ys)
+
+plt.show()
