718542736fbf1f3556c3b94ba99136f587676943
PlayLucky <timo.koesters@gmail.com>
2018-05-11 17:38:59 +0200

    Added danger recognition, which writes a file in .danger/[IMEI]

--- Prediction/main.py
+++ Prediction/main.py
@@ -1,32 +1,26 @@
-import glob
-from vec2 import Vec2
 from collision import find_collision
-from interpolate import interpolate
+from predict import predict
+from read import read_data
+from show import show
 
-data_path = ".data/"
+danger_path = ".danger/"
 
-objects = {}
-for filename in glob.glob(data_path + "*"):
-    name = filename.lstrip(data_path)
-    with open(filename, "r") as f:
-        for position in f:
-            parts = position.split()
-            time = int(parts[0])
-            x = float(parts[1])
-            y = float(parts[2])
-            objects.setdefault(name, [])
-            objects[name].append((Vec2(x, y), time))
-
+objects = read_data()
 keys = list(objects.keys())
 
 objects_interpolated = objects
 for obj in objects_interpolated.values():
-    print(obj)
-    obj = interpolate(obj, 0.1)
+    obj.extend(predict(obj, 1, 1))
 
 for i in range(0, len(keys)):
     for j in range(i+1, len(keys)):
-        collision = find_collision(objects[keys[i]], objects[keys[j]], 1)
+        collision = find_collision(objects[keys[i]], objects[keys[j]], 0.0003)
         if collision:
-            print("ALARM ALARM ALARM")
-            print(keys[i], "and", keys[j], "collide")
+            print("COLLISION FOUND")
+            print(collision[1])
+            with open(danger_path + keys[i], "a") as myfile:
+                myfile.write(keys[i] + " " + str(collision[1]))
+            with open(danger_path + keys[j], "a") as myfile:
+                myfile.write(keys[i] + " " + str(collision[1]))
+
+show(objects)
