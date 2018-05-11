5344ca8716193282e704f73c7dc5b9d69480cb6f
PlayLucky <timo.koesters@gmail.com>
2018-05-11 17:38:05 +0200

    Reformatted php script

--- PHP-Scripts/upload.php
+++ PHP-Scripts/upload.php
@@ -6,22 +6,14 @@ $longitude = $_GET["longtitude"];
 $latitude = $_GET["latitude"];
 $time = round(microtime(true) * 1000);
 
-if($debug) {
-	echo "IMEI: " . $imei;
-	echo "<br>----------------------<br>";
-	echo "Longtitude: " . $longitude;
-	echo "<br>----------------------<br>";
-	echo "Latitude: " . $latitude;
-	echo "<br>----------------------<br>";
-	echo "Time: " . $time;
-	echo "<br>----------------------<br>";
+# TODO: Error handling
+exec("python /home/pi/development/mint/seretra/Prediction/append.py"
+  . " " . $imei
+  . " " . $time
+  . " " . $longitude
+  . " " . $latitude
+  . " 2>&1 &", $out);
 
-	// TODO: check if values are not empty
-	echo "python /home/pi/development/mint/seretra/Prediction/append.py" . " " . $imei . " " . $time . " " . $longitude . " " . $latitude . " 2>&1 &";
-}
-
-exec("python /home/pi/development/mint/seretra/Prediction/append.py" . " " . $imei . " " . $time . " " . $longitude . " " . $latitude . " 2>&1 &", $out);
-echo "<br>Outputs:<br>";
 foreach($out as $line) {
 	echo $line . "<br>";
 }
