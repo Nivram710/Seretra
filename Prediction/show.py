b7adb67efda4ead015a7bb29b1453ff7ed8ea768
timokoesters <timokoesters@protonmail.com>
2018-05-11 22:20:41 +0200

    Added missing read.py

--- Prediction/show.py
+++ Prediction/show.py
@@ -1,34 +1,20 @@
-from vec2 import Vec2
-import random
 import matplotlib.pyplot as plt
-import glob
-
-from predict import predict
-from interpolate import interpolate
-from collision import find_collision
+from read import read_data
 
 
-data_path = ".data/"
+def show(objects, points=[]):
+    for obj in objects.values():
+        obj_interpolated = obj  # interpolate(obj, 100)
+        xs = list(map(lambda d: d[0].x, obj_interpolated))
+        ys = list(map(lambda d: d[0].y, obj_interpolated))
 
-objects = {}
-for filename in glob.glob(data_path + "*"):
-    name = filename.lstrip(data_path)
-    with open(filename, "r") as f:
-        for position in f:
-            parts = position.split()
-            time = int(parts[0])
-            x = float(parts[1])
-            y = float(parts[2])
-            objects.setdefault(name, [])
-            objects[name].append((Vec2(x, y), time))
+        plt.axis("equal")
+        plt.scatter(xs, ys)
+        plt.plot(xs, ys)
 
-for obj in objects.values():
-    obj_interpolated = obj  #interpolate(obj, 100)
-    xs = list(map(lambda d: d[0].x, obj_interpolated))
-    ys = list(map(lambda d: d[0].y, obj_interpolated))
+    plt.show()
 
-    plt.axis("equal")
-    plt.scatter(xs, ys)
-    plt.plot(xs, ys)
 
-plt.show()
+if __name__ == "__main__":
+    objects = read_data()
+    show(objects)
