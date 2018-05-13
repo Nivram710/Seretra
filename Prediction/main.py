b8a0586364429363b00a95e08e6846ecc01994db
Nivram710 <contact@nivram710.de>
2018-05-13 20:40:31 +0200

    Out Varaible wurde entfernt

--- Prediction/main.py
+++ Prediction/main.py
@@ -5,22 +5,23 @@ from show import show
 
 danger_path = "/var/www/html/.danger/"
 
-objects = read_data()
-keys = list(objects.keys())
+while True:
+    objects = read_data()
+    keys = list(objects.keys())
 
-objects_interpolated = objects
-for obj in objects_interpolated.values():
-    obj.extend(predict(obj, 1, 1))
+    objects_interpolated = objects
+    for obj in objects_interpolated.values():
+        obj.extend(predict(obj, 1, 1))
 
-for i in range(0, len(keys)):
-    for j in range(i+1, len(keys)):
-        collision = find_collision(objects[keys[i]], objects[keys[j]], 0.0003)
-        if collision:
-            print("COLLISION FOUND")
-            print(collision[1])
-            with open(danger_path + keys[i], "a") as myfile:
-                myfile.write(keys[i] + " " + str(collision[1]))
-            with open(danger_path + keys[j], "a") as myfile:
-                myfile.write(keys[i] + " " + str(collision[1]))
+    for i in range(0, len(keys)):
+        for j in range(i+1, len(keys)):
+            collision = find_collision(objects[keys[i]], objects[keys[j]], 0.0003)
+            if collision:
+                print("COLLISION FOUND")
+                print(collision[1])
+                with open(danger_path + keys[i], "a") as myfile:
+                    myfile.write(keys[i] + " " + str(collision[1]))
+                with open(danger_path + keys[j], "a") as myfile:
+                    myfile.write(keys[i] + " " + str(collision[1]))
 
-show(objects)
+    show(objects)
