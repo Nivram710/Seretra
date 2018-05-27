3f18200af22631bc25bb92be63657b6d35605681
Nivram710 <juettemarvin@gmail.com>
2018-05-27 15:51:26 +0200

    Append.py unterstützt jetzt auch Höhen-Informationen

--- Prediction/append.py
+++ Prediction/append.py
@@ -3,20 +3,22 @@ import os
 
 max_datapoints = 15
 
-assert len(sys.argv) == 5
+assert len(sys.argv) == 6
 
 time = sys.argv[2]
 x = sys.argv[3]
 y = sys.argv[4]
+z = sys.argv[5]
 
 int(time)
 float(x)
 float(y)
+float(z)
 
 path = ".data/" + sys.argv[1]
 
 with open(path, "a") as f:
-    f.write(time + " " + x + " " + y + "\n")
+    f.write(time + " " + x + " " + y + " " + z + "\n")
 
 os.system("tail -n " + str(max_datapoints) + " " + path + " > " + ".data/temp")
 os.system("mv .data/temp " + path)
