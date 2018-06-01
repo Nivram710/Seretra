0d4c0e20390315e6c98daafef40c912ed3a29516
timokoesters <timokoesters@protonmail.com>
2018-06-01 18:06:38 +0200

    Only compare collision values in the future

--- Prediction/collision.py
+++ Prediction/collision.py
@@ -1,8 +1,12 @@
 from vec2 import Vec2
+import time
 
 
 # Needs constant time step
 def find_collision(o1, o2, threshold):
+    time = int(round(time.time() * 1000))
     for pos1, pos2 in zip(o1, o2):
-        if(Vec2.distance(pos1[0], pos2[0]) < threshold):
+        if pos1[1] < time and pos2[1] < time:
+            continue
+        if Vec2.distance(pos1[0], pos2[0]) < threshold:
             return pos1
