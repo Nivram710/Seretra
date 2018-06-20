dc7a509ff37e07ea4688a87ab89d13783299c069
Nivram710 <contact@nivram710.de>
2018-06-20 16:44:31 +0200

    Variable der momentanen Zeit umbenannt
    "time" ist für Python reserviert

--- Prediction/collision.py
+++ Prediction/collision.py
@@ -4,9 +4,9 @@ import time
 
 # Needs constant time step
 def find_collision(o1, o2, threshold):
-    time = int(round(time.time() * 1000))
+    current_time = int(round(time.time() * 1000))
     for pos1, pos2 in zip(o1, o2):
-        if pos1[1] < time and pos2[1] < time:
+        if pos1[1] < current_time and pos2[1] < current_time:
             continue
         if Vec2.distance(pos1[0], pos2[0]) < threshold:
             return pos1
